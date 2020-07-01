60e567c9e158b74e7ef3d837fc25e86e7a407796
Robin Mauritz <robinmauritzrm@gmail.com>
2020-07-01 17:47:07 +0700

    feat(arduino): add profile support

--- arduinoSource/source.ino
+++ arduinoSource/source.ino
@@ -3,16 +3,22 @@
 */
 
 const byte signature[] = {2, 4, 3, 4};
-const int pinsUsed[] = {0, 1, 2, 3, 4, 5, 6, 7};
+const int pinsUsed[] = {1, 2, 3, 5, 6, 7};
+const int prevButtonPin = 0;
+const int nextButtonPin = 4;
 const int potentiometerPin = A10;
 byte buttonState = 0;
+byte profileButtonState = 0;
 
 void setup()
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < sizeof(pinsUsed) / sizeof(pinsUsed[0]); i++)
     {
         pinMode(pinsUsed[i], INPUT_PULLUP);
     }
+
+    pinMode(prevButtonPin, INPUT_PULLUP);
+    pinMode(nextButtonPin, INPUT_PULLUP);
 }
 
 void intByteArrayConv(int i, byte *buffer)
@@ -23,7 +29,7 @@ void intByteArrayConv(int i, byte *buffer)
 
 void loop()
 {
-    for (int i = 0; i < 8; i++)
+    for (int i = 0; i < sizeof(pinsUsed) / sizeof(pinsUsed[0]); i++)
     {
         int pinState = digitalRead(pinsUsed[i]);
         int prevState = bitRead(buttonState, i);
@@ -44,4 +50,7 @@ void loop()
     // }
 
     Serial.write(map(analogRead(potentiometerPin), 0, 1023, 0, 100));
+
+    Serial.write(digitalRead(prevButtonPin));
+    Serial.write(digitalRead(nextButtonPin));
 }
